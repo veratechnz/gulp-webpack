50f43cd0d274cab8dc145d66ce5b429f70e85d8f
veratech <jared@veratech.co.nz>
2020-08-03 12:49:33 +1200

    vue fix

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,23 @@
+// All of the imports/requires
 var axios = require('axios');
-
-// custom js file
+// This is a special kind of import/require for vue.js
+// Vue needs additional files and build tools so this way works
+// For our current gulp/webpack setup
+import Vue from 'vue/dist/vue.js';	
 
 (function(){
 
-	console.log('vvv');
+	console.log('vvv ttt');
 
+	console.log(axios);
+	console.log(Vue);
+
+	var app = new Vue({
+	  el: '#app',
+	  data: {
+	    message: 'Hello Vue!',
+	    another: 'how are yue???'
+	  }
+	})
 
 })(); // iffe ENDS ......
